2c26977b6f8d867ce1a5b437380a27ea4ddc0e34
Alex Shestmintsev <s_a_n_y_o_k90@mail.ru>
2014-12-08 21:20:59 +0200

    CRUD for bikes

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -1,6 +1,12 @@
 class BicyclesController < ApplicationController
+
+  before_action :bicycle_find_by_id, only: [:edit,:update,:show,:destroy]
+
   def index
-    
+    @bicycles = Bicycle.sorted_descending.all
+  end
+
+  def show
   end
 
   def new
@@ -17,9 +23,35 @@ class BicyclesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @bicycle.update(bicycle_params)
+      flash[:notice] = t('bicycle_edited', bicycle: "#{@bicycle.title}")
+      redirect_to bicycles_path
+    else
+      flash[:notice] = t('bicycle_edit_fail')
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    if @bicycle.destroy
+      flash[:notice] = t('bicycle_deleted', bicycle: "#{@bicycle.title}")
+    else
+      flash[:notice] = t('bicycle_delete_fail', doctor: "#{@bicycle.title}")
+    end
+    redirect_to bicycles_path
+  end
+
   private
     def bicycle_params
       params[:bicycle].permit(:title, :key, :status, :description)
+    end
+
+    def bicycle_find_by_id
+      @bicycle = Bicycle.find(params[:id])
     end
 
 end
--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,2 +1,4 @@
 class Bicycle < ActiveRecord::Base
+
+  scope :sorted_descending,-> {order("created_at DESC")}
 end
