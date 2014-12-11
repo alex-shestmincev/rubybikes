9e75297b82ad168e96d2017703d358e3e8f2f2d0
Alex Shestmintsev <s_a_n_y_o_k90@mail.ru>
2014-12-11 23:59:57 +0200

    fix track model

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,4 +1,8 @@
 class Bicycle < ActiveRecord::Base
 
+  has_many :tracks, foreign_key: :bicycle_id
+
+
+
   scope :sorted_descending,-> {order("created_at DESC")}
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,17 @@
 class Track < ActiveRecord::Base
 
+  belongs_to :bicycle
+
+  validates :user_id, numericality: { only_integer: true }
+  validates :bicycle_id, numericality: { only_integer: true }
+
+  validate :time_end_cannot_be_higher_time_start
+
+  def time_end_cannot_be_higher_time_start
+    if time_end.present? && time_start.present? && time_end <= time_start
+      errors.add(:time_end, 'Время конца должно быть больше времени начала')
+    end
+  end
+
   scope :sorted_descending,-> {order("created_at DESC")}
 end
