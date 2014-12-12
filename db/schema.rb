5d96c36ea0404cfd19d949907ae57b37a7f2017a
Alex Shestmintsev <s_a_n_y_o_k90@mail.ru>
2014-12-13 00:28:26 +0200

    dbmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212184757) do
+ActiveRecord::Schema.define(version: 20141212184857) do
 
   create_table "bicycles", force: true do |t|
     t.string   "title"
@@ -47,6 +47,10 @@ ActiveRecord::Schema.define(version: 20141212184757) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
