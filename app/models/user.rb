a3ac227827a6d243dbfea7f01cf9a52c87b396be
Alex Shestmintsev <s_a_n_y_o_k90@mail.ru>
2014-12-14 20:30:20 +0200

    change db

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable#, :confirmable
 
   scope :sorted_descending,-> {order("created_at DESC")}
 
