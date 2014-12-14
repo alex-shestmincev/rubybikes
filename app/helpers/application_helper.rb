08e7e929e80ed4aba3b4e955be395cac45e3fcc9
Alex Shestmintsev <s_a_n_y_o_k90@mail.ru>
2014-12-14 22:17:51 +0200

    tracks to user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,11 @@
 module ApplicationHelper
 
   def bicycles_options
-    bicycles_array = Bicycle.all.map { |bicycle| [bicycle.title, bicycle.id] }
+    Bicycle.all.map { |bicycle| [bicycle.title, bicycle.id] }
   end
 
+  def users_options
+    User.all.map { |user| [user.email, user.id] }
+  end
 
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -5,7 +5,6 @@ class Track < ActiveRecord::Base
   validates :user_id, numericality: { only_integer: true }
   validates :bicycle_id, numericality: { only_integer: true }
 
-  validate :time_end_cannot_be_higher_time_start
 
   scope :sorted_descending,-> {order("created_at DESC")}
 end
