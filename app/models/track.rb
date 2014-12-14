aa9f41caf8ac7759ce2f549783a689b458ba38f1
Alex Shestmintsev <s_a_n_y_o_k90@mail.ru>
2014-12-14 22:11:30 +0200

    fix model

--- app/models/track.rb
+++ app/models/track.rb
@@ -7,11 +7,5 @@ class Track < ActiveRecord::Base
 
   validate :time_end_cannot_be_higher_time_start
 
-  def time_end_cannot_be_higher_time_start
-    if time_end.present? && time_start.present? && time_end <= time_start
-      errors.add(:time_end, 'Время конца должно быть больше времени начала')
-    end
-  end
-
   scope :sorted_descending,-> {order("created_at DESC")}
 end
